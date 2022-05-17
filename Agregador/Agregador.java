fb3054ac249095e628fb39fffcc619c7cc288cc8
43Clock <luiscarlosm2000@gmail.com>
2022-05-17 19:17:58 +0100

    Comunicação entre agregadores

--- Agregador/Agregador.java
+++ Agregador/Agregador.java
@@ -4,40 +4,52 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Agregador {
-    protected int zona;
-    protected int superZona;
-    protected Set<String> dispositivosOnline;
-    protected Map<String,Set<String>> tipos;
+    private int zona;
+    private List<Integer> vizinhos;
+    private Set<String> dispositivosOnline;
+    private Map<String,Set<String>> tipos;
 
 
-    public Agregador(Set<String> dispositiovosOnline,Map<String,Set<String>> tipos, String zona, String superZona){
-        this.dispositivosOnline = dispositiovosOnline;
-        this.tipos = tipos;
+    public Agregador(String zona,List<Integer> vizinhos){
+        this.dispositivosOnline = new HashSet<String>();
+        this.tipos = new HashMap<String,Set<String>>();
         this.zona = Integer.parseInt(zona);
-        this.superZona = Integer.parseInt(superZona);
+        this.vizinhos = new ArrayList<>();
+        for (int v: vizinhos)
+            this.vizinhos.add(v);
     }
 
     public void start(){
         try (ZContext context = new ZContext();
              ZMQ.Socket fromColector = context.createSocket(SocketType.PULL);
-             ZMQ.Socket toAgregador = context.createSocket(SocketType.REQ))
+             ZMQ.Socket inform = context.createSocket(SocketType.PUB);
+             ZMQ.Socket receive = context.createSocket(SocketType.SUB))
         {
             int portaColetor = 3002 + this.zona*100;
-            int portaSuper = 3003 + this.zona*100;
+            int portaAgregador = 3003 + this.zona*100;
             fromColector.bind("tcp://*:"+portaColetor);
-            toAgregador.connect("tcp://localhost:"+portaSuper);
+            receive.bind("tcp://*:"+portaAgregador);
+            receive.subscribe("".getBytes());
+            Thread t = new UpdatesHandler(zona, vizinhos, dispositivosOnline, tipos, receive, inform);
+            t.start();
+            
+            for(int v : this.vizinhos)
+                inform.connect("tcp://localhost:"+(3003+v*100));
             while(true){
                 byte[] msg = fromColector.recv();
                 String str = new String(msg);
                 if (this.processMessage(str)){
                     System.out.println("Send to Super Node to update");
+                    inform.send("Login");
                 }
                 System.out.println("Dispositivos:"+this.dispositivosOnline);
                 System.out.println("Tipos:"+this.tipos);
@@ -83,3 +95,31 @@ public class Agregador {
     }
 }
 
+class UpdatesHandler extends Thread {
+    private int zona;
+    private List<Integer> vizinhos;
+    private Set<String> dispositivosOnline;
+    private Map<String,Set<String>> tipos;
+    private ZMQ.Socket receive;
+    private ZMQ.Socket inform;
+    
+    UpdatesHandler(int zona, List<Integer> vizinhos,Set<String> dispositivosOnline
+                  ,Map<String,Set<String>> tipos,ZMQ.Socket receive,ZMQ.Socket inform)
+    {
+        this.zona = zona;
+        this.vizinhos = vizinhos;
+        this.dispositivosOnline = dispositivosOnline;
+        this.tipos = tipos;
+        this.receive = receive;
+        this.inform = inform;
+    }
+    
+    public void run(){
+        while(true){
+            byte[] msg = this.receive.recv();
+            String str = new String(msg);
+            System.out.println(str);
+        }
+    }
+
+}
--- Agregador/AgregadorRun.java
+++ Agregador/AgregadorRun.java
@@ -1,5 +1,6 @@
 package Agregador;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -7,20 +8,15 @@ import java.util.Set;
 
 class AgregadorRun {
     public static void main(String[] args) {
-        if(args.length < 1) {
+        if(args.length < 2) {
             System.out.println("Argumentos insuficientes!");
         } else {
-            Set<String> dispositivosOnline = new HashSet<String>();
-            Map<String,Set<String>> tipos = new HashMap<String,Set<String>>();
-            if(args.length == 2){
-                Agregador a = new Agregador(dispositivosOnline,tipos,args[0],args[1]);
-                a.start();
+            ArrayList<Integer> list = new ArrayList<Integer>();
+            for(int i = 1;i<args.length;i++){
+                list.add(Integer.parseInt(args[i]));
             }
-            else if(args.length == 1){
-                AgregadorSuper a = new AgregadorSuper(dispositivosOnline,tipos,args[0]);
-                a.run();
-                a.start();
-            }
+            Agregador a = new Agregador(args[0],list);
+            a.start();
         }
     }
 }
